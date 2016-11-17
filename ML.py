a25c6cf1cb9fbcb4815624bcfe4e777537b1b248
wh000 <weihan_sang93@hotmail.com>
2016-11-18 01:51:49 +0800

    modified simplePredictor, added get_gold and get_precision functions

--- ML.py
+++ ML.py
@@ -94,7 +94,7 @@ def transmission(f):
     print t_bn
 
 
-
+#takes in text file, returns open list of 
 def simplePredictor(f):
     counter = 0
     StateList = []
@@ -103,10 +103,11 @@ def simplePredictor(f):
         ListProb = [0,0,0,0,0,0,0]
         counter += 1
         lines = lines.strip('\n')
-        if lines != '':
+        lines = lines.split(' ')
+        if lines[0] != '':
             for i in range(7):
                 try:
-                    ListProb[i] = List[i][lines]
+                    ListProb[i] = List[i][lines[0]]
                 except:
                     pass
             if sum(ListProb) == 0:
@@ -120,6 +121,80 @@ def simplePredictor(f):
             SubList = []
             
     return(StateList)
+
+#takes in file, returns nested list of sentence states
+def get_gold_from_file(f):
+    total = []
+    sentence = []
+    for line in f:
+        if line == '\n':
+            total.append(sentence)
+            sentence = []
+            
+        else:
+            line = line.strip('\n')
+            tList = line.split(" ")
+            sentence.append(tList[1])
+    return total
+
+#takes in 2 nested lists, outputs the total precision and recall value
+def get_f(gold, predicted):
+    total_precision = 0
+    total_recall = 0
+
+    for i in range(len(gold)):
+        ith_gold = 0
+        entities_predicted = 0
+        correctly_predicted = 0
+        prev_state = 'O'
+        c_state = 0
+        for j in range(len(gold[i])):
+            #c_state 0: default/no match
+            #c_state 1: comparing entity
+            #c_state 2: entity comparison done, successful match
+
+            #gold entities in the sentence
+            if gold[i][j][0] == 'B':
+               ith_gold += 1 
+            #predicted entities
+            if (prev_state == 'O' and predicted[i][j][0] != 'O'):
+                entities_predicted += 1
+                c_state = 1
+            if c_state == 1:
+                #comparison successful, go to c_state 2
+                if (predicted[i][j][0] == 'O' and gold[i][j][0] == 'O'):
+                    c_state = 2
+                #comparing
+                else:
+                    #if its wrong, go back to c_state 0
+                    try:
+                        if gold[i][j][2:5] != predicted[i][j][2:5]:
+                            c_state = 0
+                    except:
+                        c_state = 0
+            if c_state == 2:
+                correctly_predicted += 1
+                c_state = 0
+                
+            prev_state = predicted[i][j][0]
+            
+            #if the entity is at the end of the sentence and is correct
+            if j == len(gold[i])-1 and c_state == 1:
+                correctly_predicted += 1
+        print 'total_predicted: ' + str(entities_predicted)
+        print 'correctly_predicted: ' + str(correctly_predicted)
+        print 'gold: ' + str(ith_gold)
+        print gold[i]
+        print predicted[i]
+        if entities_predicted >0:        
+            total_precision += correctly_predicted/float(entities_predicted)
+        if ith_gold > 0:
+            total_recall += correctly_predicted/float(ith_gold)
+
+    return total_precision, total_recall
+
+        
+
 #testd = {'tt' : 10,'ttt':20,'tttt':30}
 #count = 60.0
 #for i in testd:
@@ -127,14 +202,19 @@ def simplePredictor(f):
 #count = countException(testd,count,'t')
 #print testd
 #print count
-f = open('C:/Users/Stanley Loh/Desktop/Term 6 EPD/Machine Learning/Project/gitML/train.txt',"r")
+f = open('train.txt',"r")
 emission(f)
 f.close()
-f = open('C:/Users/Stanley Loh/Desktop/Term 6 EPD/Machine Learning/Project/gitML/dev.in',"r")
-print(simplePredictor(f))
-f = open('C:/Users/Stanley Loh/Desktop/Term 6 EPD/Machine Learning/Project/gitML/train.txt',"r")
-transmission(f)
+f = open('train.txt',"r")
+gold = get_gold_from_file(f)
 f.close()
+f = open('train.txt',"r")
+predicted = simplePredictor(f)
+print get_f(gold, predicted)
+f.close()
+#f = open('train.txt',"r")
+#transmission(f)
+#f.close()
 
 
 
